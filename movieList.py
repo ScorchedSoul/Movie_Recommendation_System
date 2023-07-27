fe8376eba80f66d5d4d769f610ffbd95a2f5f76b
Palak_Raj <palakrajmain@gmail.com>
2023-07-27 13:17:32 +0530

    MOVIE LIST ADDED

---
+++ movieList.py
@@ -0,0 +1,155 @@
+# LIST OF MOVIES WITH THEIR NAME , GENRE AND RATING
+
+# Sample Movie Data
+movies = [
+    {
+        "title": "Inception",
+        "genre": "Action",
+        "rating": 8.8
+    },
+    {
+        "title": "The Dark Knight",
+        "genre": "Action",
+        "rating": 9.0
+    },
+    {
+        "title": "Gladiator",
+        "genre": "Action",
+        "rating": 8.5
+    },
+    {
+        "title": "Die Hard",
+        "genre": "Action",
+        "rating": 7.6
+    },
+    {
+        "title": "Mad Max: Fury Road",
+        "genre": "Action",
+        "rating": 8.1
+    },
+    {
+        "title": "Avengers: Endgame",
+        "genre": "Action",
+        "rating": 8.4
+    },
+    {
+        "title": "The Shawshank Redemption",
+        "genre": "Drama",
+        "rating": 9.3
+    },
+    {
+        "title": "Forrest Gump",
+        "genre": "Drama",
+        "rating": 8.8
+    },
+    {
+        "title": "The Godfather",
+        "genre": "Drama",
+        "rating": 9.2
+    },
+    {
+        "title": "Schindler's List",
+        "genre": "Drama",
+        "rating": 8.9
+    },
+    {
+        "title": "The Revenant",
+        "genre": "Drama",
+        "rating": 8.0
+    },
+    {
+        "title": "La La Land",
+        "genre": "Drama",
+        "rating": 8.0
+    },
+    {
+        "title": "The Hangover",
+        "genre": "Comedy",
+        "rating": 7.7
+    },
+    {
+        "title": "Superbad",
+        "genre": "Comedy",
+        "rating": 7.6
+    },
+    {
+        "title": "Bridesmaids",
+        "genre": "Comedy",
+        "rating": 6.8
+    },
+    {
+        "title": "Deadpool",
+        "genre": "Comedy",
+        "rating": 8.0
+    },
+    {
+        "title": "Zombieland",
+        "genre": "Comedy",
+        "rating": 7.6
+    },
+    {
+        "title": "Ted",
+        "genre": "Comedy",
+        "rating": 6.9
+    },
+    {
+        "title": "Interstellar",
+        "genre": "Sci-Fi",
+        "rating": 8.6
+    },
+    {
+        "title": "The Matrix",
+        "genre": "Sci-Fi",
+        "rating": 8.7
+    },
+    {
+        "title": "Blade Runner 2049",
+        "genre": "Sci-Fi",
+        "rating": 8.0
+    },
+    {
+        "title": "Star Wars: Episode IV - A New Hope",
+        "genre": "Sci-Fi",
+        "rating": 8.6
+    },
+    {
+        "title": "In Time",
+        "genre": "Sci-Fi",
+        "rating": 6.7
+    },
+    {
+        "title": "E.T. the Extra-Terrestrial",
+        "genre": "Sci-Fi",
+        "rating": 7.8
+    },
+    {
+        "title": "The Lord of the Rings: The Fellowship of the Ring",
+        "genre": "Fantasy",
+        "rating": 8.8
+    },
+    {
+        "title": "Harry Potter and the Sorcerer's Stone",
+        "genre": "Fantasy",
+        "rating": 7.6
+    },
+    {
+        "title": "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+        "genre": "Fantasy",
+        "rating": 6.9
+    },
+    {
+        "title": "Pan's Labyrinth",
+        "genre": "Fantasy",
+        "rating": 8.2
+    },
+    {
+        "title": "Stardust",
+        "genre": "Fantasy",
+        "rating": 7.6
+    },
+    {
+        "title": "The Princess Bride",
+        "genre": "Fantasy",
+        "rating": 8.1
+    }
+]
